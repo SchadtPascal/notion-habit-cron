bf04947fdc86e234b43ba598c1c6959124c46dd5
SchadtPascal <pascal.schadt2000@gmail.com>
2025-07-20 14:30:04 +0200

    Update habit_reset.py

--- habit_reset.py
+++ habit_reset.py
@@ -11,9 +11,17 @@ def update_habits():
     response = notion.databases.query(database_id=database_id)
     for page in response["results"]:
         props = page["properties"]
-        
-        # Nur bearbeiten, wenn Checkbox "Erledigt" aktiviert ist
-        if props["Erledigt"]["checkbox"]:
+
+        # Prüfen auf: Erledigt + Kategorie == Habits
+        is_done = props["Erledigt"]["checkbox"]
+        is_habit = (
+            "Kategorie" in props and
+            props["Kategorie"]["type"] == "select" and
+            props["Kategorie"]["select"] and
+            props["Kategorie"]["select"]["name"] == "Habits"
+        )
+
+        if is_done and is_habit:
             old_date = props["Datum"]["date"]["start"]
             new_date = (datetime.fromisoformat(old_date).date() + timedelta(days=1)).isoformat()
             
@@ -28,4 +36,5 @@ def update_habits():
 
 if __name__ == "__main__":
     update_habits()
+
 
