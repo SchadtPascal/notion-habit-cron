84d41985f57c4b06e32870da83f8c1805eb8608e
SchadtPascal <pascal.schadt2000@gmail.com>
2025-07-21 09:01:42 +0200

    Update habit_reset.py

--- habit_reset.py
+++ habit_reset.py
@@ -7,25 +7,19 @@ notion = Client(auth=os.environ["NOTION_TOKEN"])
 database_id = os.environ["NOTION_DB_ID"]
 
 def update_habits():
-    # Abfrage aller Einträge in der Notion-Datenbank
     response = notion.databases.query(database_id=database_id)
     for page in response["results"]:
         props = page["properties"]
 
-        # Prüfen auf: Erledigt + Kategorie == Habits
-        is_done = props["Erledigt"]["checkbox"]
-        is_habit = (
-            "Kategorie" in props and
-            props["Kategorie"]["type"] == "select" and
-            props["Kategorie"]["select"] and
-            props["Kategorie"]["select"]["name"] == "Habits"
-        )
+        erledigt = props.get("Erledigt", {}).get("checkbox", False)
+        gruppe = props.get("Gruppe", {}).get("rich_text", [])
+        gruppe_text = gruppe[0]["text"]["content"] if gruppe else ""
 
-        if is_done and is_habit:
+        # Nur wenn "Erledigt" aktiviert ist und Gruppe == "Habits"
+        if erledigt and gruppe_text == "Habits":
             old_date = props["Datum"]["date"]["start"]
             new_date = (datetime.fromisoformat(old_date).date() + timedelta(days=1)).isoformat()
-            
-            # Update: Datum +1 Tag & Checkbox zurücksetzen
+
             notion.pages.update(
                 page_id=page["id"],
                 properties={
@@ -36,5 +30,3 @@ def update_habits():
 
 if __name__ == "__main__":
     update_habits()
-
-
